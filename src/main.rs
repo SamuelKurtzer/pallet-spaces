6d54b637bec54196cb2c9801f0213e50bded9dcb
Samuel Kurtzer <email@samuelkurtzer.com>
2025-03-30 10:19:33 +1030

    Adding initial hello world in axum

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {"hello world"}));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:37373").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
