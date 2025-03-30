a2a43a4467f5d06fad04f7e85fa9ff6aa2a11435
Samuel Kurtzer <email@samuelkurtzer.com>
2025-03-30 14:28:49 +1030

    adding initial front end code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::{get, get_service}, Router};
+use tower_http::services::ServeDir;
+use std::net::SocketAddr;
+
+async fn handler_404() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not Found")
+}
+
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {"hello world"}));
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("../frontend"))
+        .fallback(handler_404);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:37373").await.unwrap();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 37373));
+
+    println!("Serving app at: http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
